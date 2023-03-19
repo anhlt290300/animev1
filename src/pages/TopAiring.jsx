fb34d8fab50f05055faf02d38d56f3e3431d5191
anhlt290300 <70874794+anhlt290300@users.noreply.github.com>
2023-03-19 16:19:36 +0700

    abc

--- src/pages/TopAiring.jsx
+++ src/pages/TopAiring.jsx
@@ -4,8 +4,15 @@ import ListCards from "../component/ListCards";
 const TopAiring = () => {
   return (
     <div className="text-white">
-      <h1 className=" inline-block border-b-2 border-red-500 desktop-L:text-3xl tablet:text-2xl mobile-L:text-base text-sm font-semibold">TOP AIRING</h1>
+      <h1 className=" inline-block border-b-2 border-red-500 desktop-L:text-3xl tablet:text-2xl mobile-L:text-base text-sm font-semibold">
+        TOP AIRING
+      </h1>
       <ListCards topairing={true} />
+      <img
+        src="https://cdn.hencb.top/ext/2021/2/3/yeu-cau-hu-hong-bi-mat/1/01.jpg"
+        referrerPolicy="no-referrer"
+        alt=""
+      />
     </div>
   );
 };
